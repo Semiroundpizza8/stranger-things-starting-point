d9b3c01c7a36efcacf46775e9a2f3cee823f3b5e
SravanthiN4 <snedunoori44@gmail.com>
2022-03-09 20:02:58 -0600

    added files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import PostForm from "./PostForm";
 import PostList from "./PostList";
 
 const App = (props) => {
   const { name } = props;
+
   return (
     <>
+      <div>
+        <h1>Hot Reload is working?</h1>
+      </div>
       <h1>Welcome, {name}</h1>
+      <PostForm />
       <PostList />
     </>
   );
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,56 @@
 export const getPosts = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/posts/';
-    const response = await fetch(url)
-    const json = await response.json()
-    return json;
-} 
+	// URL that we're gonna reach out to
+	const url = 'https://jsonplaceholder.typicode.com/posts/';
+
+	// Grab the body given back by the API
+	const response = await fetch(url);
+	console.log(response);
+
+	// Take the body we got back and convert it to JS Object
+	const json = await response.json();
+	console.log(json);
+
+	return json;
+};
+
+export const createNewPost = async (newPost) => {
+	const url = 'https://jsonplaceholder.typicode.com/posts/';
+	const response = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(newPost)
+	});
+
+	const json = await response.json();
+	console.log(json);
+	return json;
+};
+
+export const updateNewPost = async (newPost) => {
+	const url = 'https://jsonplaceholder.typicode.com/posts/';
+	const response = await fetch(url, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(newPost)
+	});
+
+	const json = await response.json();
+	console.log(json);
+	return json;
+};
+
+export const deletePostById = async (postId) => {
+	const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+
+	const response = await fetch(url, {
+		method: 'DELETE'
+	});
+
+	const json = await response.json();
+	console.log(json);
+	return json;
+};
