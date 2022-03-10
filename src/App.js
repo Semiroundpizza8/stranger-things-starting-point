39dd1925786f56b7ef0b3cd4db76c5075975171f
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-03-09 19:16:10 -0600

    Finished adding post form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import PostForm from "./PostForm";
 import PostList from "./PostList";
 
 const App = (props) => {
   const { name } = props;
+
   return (
     <>
+      <div>
+        <h1>Hot Reload is working?</h1>
+      </div>
       <h1>Welcome, {name}</h1>
+      <PostForm />
       <PostList />
     </>
   );
 }
 
 export default hot(App);
+
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,30 @@
 export const getPosts = async () => {
+    // URL that we're gonna reach out to
     const url = 'https://jsonplaceholder.typicode.com/posts/';
-    const response = await fetch(url)
-    const json = await response.json()
+
+    // Grab the body given back by the API
+    const response = await fetch(url);
+    console.log(response)
+
+    // Take the body we got back and convert it to JS Object
+    const json = await response.json();
+    console.log(json)
+
     return json;
-} 
+};
+
+
+export const createNewPost = async (newPost) => {
+    const url = 'https://jsonplaceholder.typicode.com/posts/';
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPost)
+    });
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+};
