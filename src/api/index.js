5fd43146f8e808e6aa31163752b83807633670ad
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-03-09 19:40:23 -0600

    Added api functions for update and delete

--- src/api/index.js
+++ src/api/index.js
@@ -28,3 +28,30 @@ export const createNewPost = async (newPost) => {
     console.log(json);
     return json;
 };
+
+export const updateNewPost = async (newPost) => {
+    const url = 'https://jsonplaceholder.typicode.com/posts/';
+    const response = await fetch(url, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPost)
+    });
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+};
+
+export const deletePostById = async (postId) => {
+    const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+
+    const response = await fetch(url, {
+        method: "DELETE"
+    });
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+};
