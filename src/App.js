172b59c738061710b53a3ce89665188870081bd8
SravanthiN4 <snedunoori44@gmail.com>
2022-03-15 11:47:23 +0530

    new code

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import UserRegistration from './UserRegistration';
 import Login from './Login';
 import styles from './RegistrationStyles.styles';
-// import AboutMe from './AboutMe';
 import { logOutUser } from './api';
 
 
 const App = () => {
-	// const { name } = props;
 	const [isLoggedIn,setIsLoggedIn] = useState(false);
 	const handleLogout = () => {
     
@@ -27,32 +25,19 @@ const App = () => {
 				{!isLoggedIn && <Route path="/register">
 					<UserRegistration isLoggedIn = {isLoggedIn} setIsLoggedIn = {setIsLoggedIn} />
 				</Route>}
-				
-				
-				{/* <Link to="/login">Login</Link> */}
 
 				{!isLoggedIn && <Route path="/login">
 					<Login isLoggedIn = {isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
 					
 				</Route> }
 
-				
-
 				{isLoggedIn && <Route path="/posts">
 					<PostList isLoggedIn = {isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
 					
 				</Route> }
 
-
-
-
-				
-
-				{/* {!isLoggedIn ?  <Link to = "/logout">LogOut</Link> : <UserRegistration/>} */}
-
 				{isLoggedIn ? <><Link to = "/logout" onClick={handleLogout}>Logout</Link> <Link to ="/posts">Posts</Link></>: <><Link to="/register">Register</Link><Link to="/login">Login</Link></>}
 
-				
 				
 			</BrowserRouter>
 		</div>
--- src/Login.js
+++ src/Login.js
@@ -1,104 +1,71 @@
-import React, { useState } from "react";
-import { loginUser} from "./api";
-import styles from "./RegistrationStyles.styles";
-
-
-
+import React, { useState } from 'react';
+import { loginUser } from './api';
+import styles from './RegistrationStyles.styles';
 
 const Login = (props) => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  
-  const [hasTriggeredError,setHasTriggeredError] = useState(false);
-  const {isLoggedIn,setIsLoggedIn} = props;
-  
-  const handleSubmitButton = async (event) => {
-    event.preventDefault();
-    console.log("Making a submit request...");
-    
-    const dummyBody = {
-      user: {
-      username: userName,
-      password: password
-      
-      },
+	const [ userName, setUserName ] = useState('');
+	const [ password, setPassword ] = useState('');
 
-    };
+	const [ hasTriggeredError, setHasTriggeredError ] = useState(false);
+	const { setIsLoggedIn } = props;
 
-    if(userName.length < 5) {
-      setHasTriggeredError(true);
-    } 
+	const handleSubmitButton = async (event) => {
+		event.preventDefault();
+		console.log('Making a submit request...');
 
-    if(password.length < 5) {
-      setHasTriggeredError(true)
-    }
+		const dummyBody = {
+			user: {
+				username: userName,
+				password: password
+			}
+		};
 
-  
-  
-    
-    else {
+		if (userName.length < 5) {
+			setHasTriggeredError(true);
+		}
 
-      try {
-        await loginUser(dummyBody);
-      setIsLoggedIn(true);
+		if (password.length < 5) {
+			setHasTriggeredError(true);
+		} else {
+			try {
+				await loginUser(dummyBody);
+				setIsLoggedIn(true);
 
+				setUserName('');
+				setPassword('');
+			} catch (error) {
+				console.error(error);
+			}
+		}
 
-    setUserName("");
-    setPassword("");
-        
-      } catch (error) {
-        console.error(error);
-      }
+		console.log(userName);
+	};
 
-      
-    
-    }
+	const handleUserName = (event) => {
+		setUserName(event.target.value);
+	};
+	const handlePassword = (event) => {
+		setPassword(event.target.value);
+	};
 
-    console.log(userName);
+	if (hasTriggeredError) return <p style={{ color: 'red' }}> Whoopse, looks like you need to fix something! </p>;
 
-  };
+	return (
+		<div style={styles.loginStyle}>
+			<label>Username</label>
+			<input value={userName} onChange={handleUserName} required />
 
-  const handleUserName = (event) => {
-    setUserName(event.target.value);
-  }
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
-  }
-  
+			<label>Password</label>
+			<input value={password} type="password" onChange={handlePassword} required />
 
-  
+			{hasTriggeredError && <p style={{ color: 'red' }}> Whoopse, looks like you need to fix something! </p>}
 
-  if(hasTriggeredError) return <p style={{ color: 'red' }}> Whoopse, looks like you need to fix something! </p>
-
-  
-
-  return (
-    
-    <div style={styles.loginStyle}>
-      
-      <label>Username</label>
-      <input value={userName} onChange={handleUserName} required />
-
-      <label>Password</label>
-      <input value={password} type="password" onChange={handlePassword} required />
-
-     
-
-      {hasTriggeredError && <p style={{ color: 'red' }}> Whoopse, looks like you need to fix something! </p>}
-
-      <button onClick={handleSubmitButton}>
-        Login
-      </button>
-
-      
-
-    </div>
-    
-  );
+			<button onClick={handleSubmitButton}>Login</button>
+		</div>
+	);
 };
 
 export default Login;
-
 
 // button onClick={() => {
 //   const dummyCreds = {
